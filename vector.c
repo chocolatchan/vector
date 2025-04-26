c3573d7093ac6ce1bd9e9ab91aba79d8d4d8a5f1
chocolatchan <kimizukn@gmail.com>
2025-04-27 02:03:53 +0700

    minor change

--- vector.c
+++ vector.c
@@ -47,12 +47,6 @@ void node_destroy(node_t *node, void (*destroyer)(void*)) {
 
 #pragma region vector
 
-/**
- * @brief Initialize a vector on the heap with the given size.
- * @param size The size of the data in each node.
- * @return A pointer to the initialized vector.
- * @note The vector will be empty after this function is called.
- */
 vector_t *vector_init_h(size_t size) {
     vector_t *vector = (vector_t*) malloc(sizeof(vector_t));
     if (vector == NULL) {
@@ -68,12 +62,6 @@ vector_t *vector_init_h(size_t size) {
     return vector;
 }
 
-/**
- * @brief Initialize a vector on stack with the given size.
- * @param vector The vector to initialize.
- * @param size The size of the data in each node.
- * @note The vector will be empty after this function is called.
- */
 void vector_init_s(vector_t *vector, size_t size) {
     if (vector == NULL) {
         return;
@@ -85,11 +73,6 @@ void vector_init_s(vector_t *vector, size_t size) {
     vector->is_heap = 0;
 }
 
-/**
- * @brief Clear the vector and all its nodes without checking for NULL.
- * @param vector The vector to clear.
- * @param destroyer The function to call to destroy the data in each node.
- */
 static void __vector_clear_all_unchk(vector_t *vector, void (*destroyer)(void*)) {
     node_t *current = vector->head;
     while (current != NULL) {
@@ -99,14 +82,6 @@ static void __vector_clear_all_unchk(vector_t *vector, void (*destroyer)(void*))
     }
 }
 
-/**
- * @brief Clear the vector and all its nodes.
- * @param vector The vector to clear.
- * @param destroyer The function to call to destroy the data in each node.
- * @note If destroyer is NULL, the data in each node will not be destroyed.
- * @note If destroyer is not NULL, the data in each node will be destroyed using the destroyer function.
- * @note The vector will be empty after this function is called.
- */
 void vector_clear(vector_t *vector, void (*destroyer)(void*)) {
     if (vector == NULL) {
         return;
@@ -118,14 +93,6 @@ void vector_clear(vector_t *vector, void (*destroyer)(void*)) {
     vector->length = 0;
 }
 
-/**
- * @brief Destroy the vector and all its nodes.
- * @param vector The vector to destroy.
- * @param destroyer The function to call to destroy the data in each node.
- * @note If destroyer is NULL, the data in each node will not be destroyed.
- * @note If destroyer is not NULL, the data in each node will be destroyed using the destroyer function.
- * @note The vector will be NULL after this function is called.
- */
 void vector_destroy(vector_t *vector, void (*destroyer)(void*)) {
     if (vector == NULL) {
         return;
@@ -135,13 +102,6 @@ void vector_destroy(vector_t *vector, void (*destroyer)(void*)) {
     if (vector->is_heap) free(vector);
 }
 
-/**
- * @brief Append a new node to the vector.
- * @param vector The vector to append the node to.
- * @param data The data to insert into the node.
- * @param cloner The function to call to clone the data in the node.
- * @note If the vector is empty, the node will be prepended.
- */
 void vector_append(vector_t *vector, void *__restrict__ data, void *(*cloner)(void*)) {
     if (vector == NULL) {
         return;
@@ -160,13 +120,6 @@ void vector_append(vector_t *vector, void *__restrict__ data, void *(*cloner)(vo
     vector->length++;
 }
 
-/**
- * @brief Prepend a new node to the vector.
- * @param vector The vector to prepend the node to.
- * @param data The data to insert into the node.
- * @param cloner The function to call to clone the data in the node.
- * @note If the vector is empty, the node will be appended.
- */
 void vector_prepend(vector_t *vector, void *__restrict__ data, void *(*cloner)(void*)) {
     if (vector == NULL) {
         return;
@@ -185,7 +138,7 @@ void vector_prepend(vector_t *vector, void *__restrict__ data, void *(*cloner)(v
     vector->length++;
 }
 
-node_t *__vector_at_unchk(vector_t *vector, size_t index) {
+static node_t *__vector_at_unchk(vector_t *vector, size_t index) {
     node_t *current = vector->head;
     for (size_t i = 0; i < index; i++) {
         current = current->next;
@@ -193,15 +146,6 @@ node_t *__vector_at_unchk(vector_t *vector, size_t index) {
     return current;
 }
 
-/**
- * @brief Insert a new node at the left of the given index.
- * @param vector The vector to insert the node into.
- * @param index The index to insert the node at.
- * @param data The data to insert into the node.
- * @note If the index is 0, the node will be prepended.
- * @note If the index is equal to the length of the vector, the node will be appended.
- * @note If the index is greater than the length of the vector, the node will not be inserted.
- */
 void vector_insert(vector_t *vector, size_t index, void *__restrict__ data, void *(*cloner)(void*)) {
     if (vector == NULL) {
         return;
--- vector.h
+++ vector.h
@@ -31,12 +31,69 @@ struct vector {
     int     is_heap;
 };
 
+/**
+ * @brief Initialize a vector on the heap with the given size.
+ * @param size The size of the data in each node.
+ * @return A pointer to the initialized vector.
+ * @note The vector will be empty after this function is called.
+ */
 vector_t *vector_init_h(size_t size);
+
+/**
+ * @brief Initialize a vector on stack with the given size.
+ * @param vector The vector to initialize.
+ * @param size The size of the data in each node.
+ * @note The vector will be empty after this function is called.
+ */
 void vector_init_s(vector_t *vector, size_t size);
+
+/**
+ * @brief Clear the vector and all its nodes.
+ * @param vector The vector to clear.
+ * @param destroyer The function to call to destroy the data in each node.
+ * @note If destroyer is NULL, the data in each node will not be destroyed.
+ * @note If destroyer is not NULL, the data in each node will be destroyed using the destroyer function.
+ * @note The vector will be empty after this function is called.
+ */
 void vector_clear(vector_t *vector, void (*destroyer)(void*));
+
+/**
+ * @brief Destroy the vector and all its nodes.
+ * @param vector The vector to destroy.
+ * @param destroyer The function to call to destroy the data in each node.
+ * @note If destroyer is NULL, the data in each node will not be destroyed.
+ * @note If destroyer is not NULL, the data in each node will be destroyed using the destroyer function.
+ * @note The vector will be NULL after this function is called.
+ */
 void vector_destroy(vector_t *vector, void (*destroyer)(void*));
+
+/**
+ * @brief Append a new node to the vector.
+ * @param vector The vector to append the node to.
+ * @param data The data to insert into the node.
+ * @param cloner The function to call to clone the data in the node.
+ * @note If the vector is empty, the node will be prepended.
+ */
 void vector_append(vector_t *vector, void *__restrict__ data, void *(*cloner)(void*));
+
+/**
+ * @brief Prepend a new node to the vector.
+ * @param vector The vector to prepend the node to.
+ * @param data The data to insert into the node.
+ * @param cloner The function to call to clone the data in the node.
+ * @note If the vector is empty, the node will be appended.
+ */
 void vector_prepend(vector_t *vector, void *__restrict__ data, void *(*cloner)(void*));
+
+/**
+ * @brief Insert a new node at the left of the given index.
+ * @param vector The vector to insert the node into.
+ * @param index The index to insert the node at.
+ * @param data The data to insert into the node.
+ * @note If the index is 0, the node will be prepended.
+ * @note If the index is equal to the length of the vector, the node will be appended.
+ * @note If the index is greater than the length of the vector, the node will not be inserted.
+ */
 void vector_insert(vector_t *vector, size_t index, void *__restrict__ data, void *(*cloner)(void*));
 void vector_remove(vector_t *vector, size_t index, void (*destroyer)(void*));
 void *vector_at(vector_t *vector, size_t index);
